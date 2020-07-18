620e2d4e7840d3d428ac3e13ac7c7918eed8cfd0
Kay Suparx <nitt0x0@gmail.com>
2020-07-18 18:31:40 +0900

    1.95-finish le10

--- lectures/lecture-JS/lt10-JSON.js
+++ lectures/lecture-JS/lt10-JSON.js
@@ -9,56 +9,81 @@ function ex10_0() {
 
 }
 
-// TODO:
 
-//JSON
+//JSON : stringify (obj) <--> parse to (obj)
 //JavaScript Object Notation
 
-// 1. Object to JSON
-// stringfy (OBJ)
+let json
 
-let json = JSON.stringify(true);
-console.log(json);
-
-json = JSON.stringify(['🍎', '🍌']);
-
-console.log(json);
 const rabbit = {
   name : 'Tori',
   color: 'white',
   size : null,
   birthDate : new Date(),
-  
-  // symbol: Symbol("id"),
+  // symbol: Symbol("id"),                    // Error Occure
   jump : () => {
     console.log(`This '${name}' can jump!`);
+    return name
   },
 }
 
-json = JSON.stringify(rabbit);
-console.log(json);
 
-json = JSON.stringify(rabbit, ['name', 'color', 'size',]);
-console.log(json);
 
-json = JSON.stringify(rabbit, (key, value) => {
-  console.log(`key: ${key}, value: ${value}`);
-  // return value;
-  return key == 'name' ? 'Ellie': value;
-});
-console.log(json);
+// TODO:
+ex10_2();
+ex10_1();
 
-rabbit.jump();
+
+// 1. Object to JSON
+// stringfy (OBJ)
+function ex10_1() {
+  json = JSON.stringify(true);
+  console.log(json);
+
+  json = JSON.stringify(['🍎', '🍌']);
+  console.log(json);
+
+  json = JSON.stringify(['apple', 'banana']);
+  console.log(json);
+
+  json = JSON.stringify(rabbit);
+  console.log('stringify(rabbit) =', json);
+
+  json = JSON.stringify(rabbit, ['name', 'color', 'size',]);
+  console.log('stringify(custom) =', json);
+
+  console.log('... return key:value ............');
+  json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    // return value;                              // Error = symbol("id")
+    return key == 'name' ? 'Ellie': value;         // control by arrow func.
+  });
+
+  console.log(json);
+
+  console.log('... the rabbit will jump! .......');
+  rabbit.jump();
+}
 
 // 2. JSON to Object
 // parse(json)
+function ex10_2() {
+  // console.clear();
+  json = JSON.stringify(rabbit);
+  console.log(json);
 
-console.clear();
-json = JSON.stringify(rabbit);
+  const obj = JSON.parse(json, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key == 'birthDate' ? new Date(value) : value;
+  });
 
-const obj = JSON.parse(json);
-console.log(obj);
+  console.log(obj);
 
-rabbit.jump();
+  rabbit.jump();
 
-// obj.jump();
+  // obj.jump();     // Err
+  console.log('   -->', rabbit.birthDate.getDate());
+  console.log('  -->>', obj.birthDate.getDate());
+
+  console.log(' -->>>', obj.birthDate);
+}
