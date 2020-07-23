da1bcdc0f0a3150b4e03afb6d605f9761615ec21
Kay Suparx <nitt0x0@gmail.com>
2020-07-23 12:05:43 +0900

    3.0-Open dailyCoding-JS lecture!

---
+++ lectures/lecture-dailyCoding/dc03.js
@@ -0,0 +1,15 @@
+"use strict";
+// *************************
+// dc03.js
+// *************************
+
+function getNight() {
+  document.querySelector('body').style.backgroundColor= 'black';
+  document.querySelector('body').style.color = 'orange';
+}
+
+
+function getDay() {
+  document.querySelector('body').style.backgroundColor= 'white';
+  document.querySelector('body').style.color = 'black';
+}
